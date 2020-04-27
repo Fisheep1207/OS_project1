6e6022d0bba68c8034ab78844a6faa3ab637a64b
Fisheep1207 <mike881207@gmail.com>
2020-04-26 23:39:54 -0700

    kernel_file

--- kernel_files/sys_my_printk.c
+++ kernel_files/sys_my_printk.c
@@ -1,10 +1,10 @@
 #include <linux/linkage.h>
 #include <linux/kernel.h>
 // system call number = 334
-asmlinkage void mySysPrintk(unsigned long start_sec,
-                           unsigned long start_float,
-                           unsigned long end_sec,
-                           unsigned long end_float, int pid){
+asmlinkage void mySysPrintk(unsigned long __user start_sec,
+                           unsigned long __user start_float,
+                           unsigned long __user end_sec,
+                           unsigned long __user end_float, int __user pid){
     printk( "[Project1] %d %lu.%09lu %lu.%09lu\n",
         pid, start_sec, start_float, end_sec, end_float);
     return ;
